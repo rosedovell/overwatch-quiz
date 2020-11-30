2cd41b80d87758294b83ee4fcdabe9d064bb716e
Rose Dovell <rosedovell@protonmail.com>
2020-11-30 11:53:32 -0500

    Added Reinhardt stats, tracking of quiz time, accuracy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from random import shuffle
+from time import perf_counter
 
 dva = [ 
         "D.va", 
@@ -12,7 +13,7 @@ dva = [
             "Mech Shield HP" : "0",
             "Eject Cast Time" : "1s",
             "Fusion Cannon Damage" : "0.6-2",
-            "Fusion Cannon Fire Rate" : "6.67 rps",
+            "Fusion Cannon Fire Rate" : "6.67rps",
             "Fusion Cannon Speed Drop" : "60%",
             "Defense Matrix Max Duration" : "2s",
             "Defense Matrix Recharge" : "7s",
@@ -30,7 +31,7 @@ dva = [
             "Self-Destruct Damage" : "1000",
             "Self-Destruct Time Fuse" : "3s",
             "Baby D.va Light Gun Damage" : "14",
-            "Baby D.va Light Gun Fire Rate" : "7 rps",
+            "Baby D.va Light Gun Fire Rate" : "7rps",
             "Baby D.va Light Gun Ammo" : "20",
             "Baby D.va Light Gun Reload Time" : "1.5s",
             "Call Mech Damage" : "50",
@@ -46,7 +47,7 @@ orisa = [
             "Armor HP" : "200",
             "Shield HP" : "0",
             "Fusion Driver Damage" : "11",
-            "Fusion Driver Fire Rate" : "12 rps",
+            "Fusion Driver Fire Rate" : "12rps",
             "Fusion Driver Ammo" : "150",
             "Fusion Driver Reload Time" : "2.5s",
             "Fusion Driver Speed Drop" : "70%",
@@ -74,19 +75,43 @@ reinhardt = [
         "Total HP" : "500",
         "Normal HP" : "300",
         "Armor HP" : "200",
-        "Shield HP" : "0"
+        "Shield HP" : "0",
+        "Steadfast knockback reduction" : "30%",
+        "Rocket Hammer Damage" : "75",
+        "Rocket Hammer Fire Rate" : "1.11rps",
+        "Rocket Hammer Range" : "5m",
+        "Barrier Field Shield HP" : "1600",
+        "Barrier Field Shield Regen Rate" : "195hps",
+        "Barrier Field Regen Cooldown (Not broken)" : "2s",
+        "Barrier Field Cooldown (When broken)" : "5s",
+        "Barrier Field Speed" : "70%",
+        "Charge Bump Damage" : "50",
+        "Charge Pin Damage" : "300",
+        "Charge Speed" : "300%",
+        "Charge Cooldown (Not interrupted)" : "10s",
+        "Charge Cooldown (If interrupted)" : "3s",
+        "Fire Strike Damage" : "100",
+        "Fire Strike Cast Time" : "0.6s",
+        "Fire Strike Cooldown" : "6s",
+        "Earthshatter Damage" : "50",
+        "Earthshatter Stun Time" : "2.5s",
+        "Earthshatter Cast Time" : "0.6s",
+        "Earthshatter Range" : "20m"
     }
 ]
 
 heroes = [
     dva,
-    orisa,
-    reinhardt
+#    orisa,
+#    reinhardt
     ]
 
 flashcards = {}
 flashcards_keys = []
 correct_keys = []
+questions_asked = 0.0
+
+tic = perf_counter()
 
 for hero in heroes:
     hero_name = hero[ 0 ]
@@ -103,16 +128,19 @@ for hero in heroes:
 
 while len( flashcards_keys ) != len( correct_keys ):
     shuffle( flashcards_keys )
-    questions_asked = 0
-    correct_responses = 0
     for question in flashcards_keys:
         if question not in correct_keys:
             user_response = input( question + ": " )
             questions_asked += 1
             if user_response == flashcards [ question ]:
                 print( "CORRECT!" )
-                correct_responses += 1
                 correct_keys.append( question )
             else:
                 print( "INCORRECT" )
                 print( "Correct answer was: " + flashcards[ question ] )
+
+print("")
+toc = perf_counter()
+print( "Completed in " + str(int(toc-tic)) + " seconds." )
+accuracy = "{:.2f}".format( ( len(flashcards) / questions_asked ) * 100 )
+print( "Accuracy: " + str( accuracy ) + "%" )   
