292ea5383cd2ca510aa10b0170bd0571a0070b2d
Rose Dovell <rosedovell@protonmail.com>
2020-11-30 10:30:42 -0500

    Changed quiz format to endless grind, added D.va stats

--- main.py
+++ main.py
@@ -9,7 +9,32 @@ dva = [
             "Baby D.va Total HP" : "150",
             "Mech Normal HP" : "400",
             "Mech Armor HP" : "200",
-            "Mech Shield HP" : "0"
+            "Mech Shield HP" : "0",
+            "Eject Cast Time" : "1s",
+            "Fusion Cannon Damage" : "0.6-2",
+            "Fusion Cannon Fire Rate" : "6.67 rps",
+            "Fusion Cannon Speed Drop" : "60%",
+            "Defense Matrix Max Duration" : "2s",
+            "Defense Matrix Recharge" : "7s",
+            "Defense Matrix Cooldown" : "1s",
+            "Defense Matrix Range" : "10m",
+            "Boosters Damage" : "10",
+            "Boosters Speed" : "230%",
+            "Boosters Duration" : "2s",
+            "Boosters Cooldown" : "4s",
+            "Micro Missiles Damage" : "2-7",
+            "Micro Missiles Count" : "18",
+            "Micro Missiles Duration" : "1.6s",
+            "Micro Missiles Cooldown" : "8s",
+            "Self-Destruct Area Radius" : "20m",
+            "Self-Destruct Damage" : "1000",
+            "Self-Destruct Time Fuse" : "3s",
+            "Baby D.va Light Gun Damage" : "14",
+            "Baby D.va Light Gun Fire Rate" : "7 rps",
+            "Baby D.va Light Gun Ammo" : "20",
+            "Baby D.va Light Gun Reload Time" : "1.5s",
+            "Call Mech Damage" : "50",
+            "Call Mech Cast Time" : "2s"
         }
     ]
 orisa = [ 
@@ -24,6 +49,7 @@ orisa = [
 heroes = [ dva, orisa ]
 flashcards = {}
 flashcards_keys = []
+correct_keys = []
 for hero in heroes:
     hero_name = hero[ 0 ]
     hero_questions = hero[ 1 ]
@@ -37,14 +63,18 @@ for hero in heroes:
         flashcards[ key ] = hero_questions[ question ]
         flashcards_keys.append( key )
 
-shuffle( flashcards_keys )
-questions_asked = 0
-correct_responses = 0
-for question in flashcards_keys:
-    user_response = input(question + ": ")
-    questions_asked += 1
-    if user_response == flashcards [ question ]:
-        print( "CORRECT!")
-        correct_responses += 1
-
-print( "You got " + str(correct_responses) + " out of " + str(questions_asked) + " correct.")
+while len( flashcards_keys ) != len( correct_keys ):
+    shuffle( flashcards_keys )
+    questions_asked = 0
+    correct_responses = 0
+    for question in flashcards_keys:
+        if question not in correct_keys:
+            user_response = input(question + ": ")
+            questions_asked += 1
+            if user_response == flashcards [ question ]:
+                print( "CORRECT!" )
+                correct_responses += 1
+                correct_keys.append( question )
+            else:
+                print( "INCORRECT" )
+                print( "Correct answer was: " + flashcards[ question ] )
